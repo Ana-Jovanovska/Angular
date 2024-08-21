3d6126bea1863f616b60f6be975f512ec0aecfc4
Ana-Jovanovska <150249028+Ana-Jovanovska@users.noreply.github.com>
2024-08-21 19:41:05 +0200

    Update

--- Homework - 01/job-seeker/src/app/core/directives/toggle-jobs.directive.ts
+++ Homework - 01/job-seeker/src/app/core/directives/toggle-jobs.directive.ts
@@ -1,9 +1,46 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appToggleJobs]',
   standalone: true,
 })
-export class ToggleJobsDirective {
-  constructor() {}
+export class ToggleJobsDirective implements OnInit {
+  private elementRef = inject(ElementRef);
+  private renderer = inject(Renderer2);
+
+  isInitiallyClick = input(false);
+  addJobsDetails: HTMLDivElement;
+  isClick = false;
+
+  ngOnInit(): void {
+    console.log('directive create');
+    this.addJobsDetails =
+      this.elementRef.nativeElement.querySelector('.buttonApply');
+
+    this.isClick = this.isInitiallyClick();
+
+    this.renderer.setStyle(
+      this.addJobsDetails,
+      'display',
+      this.isClick ? 'block' : 'none'
+    );
+  }
+
+  @HostListener('click') onClick() {
+    console.log('element clicked');
+    this.isClick = !this.isClick;
+    this.renderer.setStyle(
+      this.addJobsDetails,
+      'display',
+      this.isClick ? 'block' : 'none'
+    );
+  }
 }
